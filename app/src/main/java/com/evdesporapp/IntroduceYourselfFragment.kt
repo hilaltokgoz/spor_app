ff913924644a8955a0f779bb03dbffb58f0e2e8b
Hilal <hilaltokgoz98@gmail.com>
2022-10-21 17:38:26 +0300

    DatePicker created

--- app/src/main/java/com/evdesporapp/IntroduceYourselfFragment.kt
+++ app/src/main/java/com/evdesporapp/IntroduceYourselfFragment.kt
@@ -7,6 +7,10 @@ import android.view.ViewGroup
 import android.widget.ArrayAdapter
 import androidx.fragment.app.Fragment
 import com.evdesporapp.databinding.FragmentIntroduceYourselfBinding
+import com.google.android.material.datepicker.CalendarConstraints
+import com.google.android.material.datepicker.MaterialDatePicker
+import java.util.*
+
 
 class IntroduceYourselfFragment : Fragment() {
     private var _binding: FragmentIntroduceYourselfBinding? = null
@@ -24,12 +28,50 @@ class IntroduceYourselfFragment : Fragment() {
         savedInstanceState: Bundle?
     ): View {
         _binding = FragmentIntroduceYourselfBinding.inflate(inflater, container, false)
+        getDate()
         return binding.root
     }
-
 
     override fun onDestroyView() {
         super.onDestroyView()
         _binding = null
     }
+
+    private fun getDate() {
+
+        //Calendar with Date Limiter
+        val calendar = Calendar.getInstance(TimeZone.getTimeZone("UTC"))
+        val today = MaterialDatePicker.todayInUtcMilliseconds()
+        calendar.timeInMillis = today
+        calendar[Calendar.YEAR] = 1950
+        val startDate = calendar.timeInMillis
+
+        calendar.timeInMillis = today
+        calendar[Calendar.YEAR] = 2010
+        val endDate = calendar.timeInMillis
+
+        val constraints: CalendarConstraints = CalendarConstraints.Builder()
+            .setOpenAt(endDate)
+            .setStart(startDate)
+            .setEnd(endDate)
+            .build()
+
+        //MaterialDatePicker
+        val datePicker =
+            MaterialDatePicker.Builder.datePicker()
+                .setTitleText("Doğum Tarihini Seçiniz")
+                .setSelection(MaterialDatePicker.todayInUtcMilliseconds()) //varsayılan olarak bugünü seçer.
+                .setInputMode(MaterialDatePicker.INPUT_MODE_TEXT)
+                .setCalendarConstraints(constraints)
+                .build()
+
+        val datePickerButton = binding.datePickerButton
+        datePickerButton.setOnClickListener {
+            datePicker.show(parentFragmentManager, "DATE_PICKER")
+        }
+        datePicker.addOnPositiveButtonClickListener {
+            datePickerButton.text = datePicker.headerText
+        }
+    }
 }
+
