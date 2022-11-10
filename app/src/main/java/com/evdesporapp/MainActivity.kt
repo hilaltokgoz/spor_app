730dd3a9dcad534c922eb5525de1979acd80726d
Hilal <hilaltokgoz98@gmail.com>
2022-11-10 21:48:29 +0300

    bottomMenu Create

--- app/src/main/java/com/evdesporapp/MainActivity.kt
+++ app/src/main/java/com/evdesporapp/MainActivity.kt
@@ -1,22 +1,17 @@
 package com.evdesporapp
 
-import android.content.Context
 import android.os.Bundle
-import android.os.Handler
-import android.util.AttributeSet
 import android.view.Menu
-import android.view.View
 import android.widget.Toast
 import androidx.appcompat.app.AppCompatActivity
-import androidx.navigation.NavController
-import androidx.navigation.Navigation
+import androidx.fragment.app.Fragment
 import androidx.navigation.fragment.NavHostFragment
 import androidx.navigation.ui.NavigationUI.setupWithNavController
-import androidx.navigation.ui.setupActionBarWithNavController
 import com.evdesporapp.databinding.ActivityMainBinding
+import com.evdesporapp.exercises.ExerciseHomeFragment
+import com.evdesporapp.fitrecipe.FitRecipeHomeFragment
+import com.evdesporapp.plans.PlanHomeFragment
 import com.google.android.material.floatingactionbutton.FloatingActionButton
-import kotlinx.coroutines.delay
-import java.util.*
 
 class MainActivity : AppCompatActivity() {
     private lateinit var binding: ActivityMainBinding
@@ -28,19 +23,45 @@ class MainActivity : AppCompatActivity() {
         binding = ActivityMainBinding.inflate(layoutInflater)
         setContentView(binding.root)
 
+        loadFragment(IntroduceYourselfFragment())
+
         //navController
-        val  navHostFragment= supportFragmentManager.findFragmentById(R.id.nav_host_fragment) as NavHostFragment
+        val navHostFragment =
+            supportFragmentManager.findFragmentById(R.id.nav_host_fragment) as NavHostFragment
         val navController = navHostFragment.navController
-        setupWithNavController(binding.bottomNavigation,navController)
+        setupWithNavController(binding.bottomNavigation, navController)
 
         binding.apply {
-            bottomNavigation.background=null
-          //  bottomNavigation.menu.getItem(2).isEnabled=false
+            bottomNavigation.background = null
+            //  bottomNavigation.menu.getItem(2).isEnabled=false
 
-            floatingActionButton= fab
+            bottomNavigation.setOnItemSelectedListener {
+                when (it.itemId) {
+                    R.id.home -> {
+                        loadFragment(HomeFragment())
+                        return@setOnItemSelectedListener true
+                    }
+                    R.id.plans -> {
+                        loadFragment(PlanHomeFragment())
+                        return@setOnItemSelectedListener true
+                    }
+                    R.id.exercise -> {
+                        loadFragment(ExerciseHomeFragment())
+                        return@setOnItemSelectedListener true
+                    }
+                    R.id.fitFood -> {
+                        loadFragment(FitRecipeHomeFragment())
+                        return@setOnItemSelectedListener true
+                    }
+                }
+                false
+            }
+
+
+            floatingActionButton = fab
             floatingActionButton.setOnClickListener {
                 //showing a toast message when clicked
-              Toast.makeText(this@MainActivity,"Clicked",Toast.LENGTH_SHORT).show()
+                Toast.makeText(this@MainActivity, "Clicked", Toast.LENGTH_SHORT).show()
             }
 
         }
@@ -49,11 +70,16 @@ class MainActivity : AppCompatActivity() {
     }
 
 
-
-
     override fun onCreateOptionsMenu(menu: Menu): Boolean {
         val inflater = menuInflater
         inflater.inflate(R.menu.bottom_nav_menu, menu)
         return true
+    }
+
+    private fun loadFragment(fragment: Fragment) {
+        val transaction = supportFragmentManager.beginTransaction()
+        transaction.replace(R.id.nav_host_fragment, fragment)
+        transaction.addToBackStack(null)
+        transaction.commit()
     }
 }
