62ae1e282ef8b6d089b00bc0c3de3a296abaa15d
Hilal <hilaltokgoz98@gmail.com>
2022-11-13 22:10:31 +0300

    customDialog

--- app/src/main/java/com/evdesporapp/mainscreen/HomeFragment.kt
+++ app/src/main/java/com/evdesporapp/mainscreen/HomeFragment.kt
@@ -1,10 +1,16 @@
 package com.evdesporapp.mainscreen
 
+import android.app.AlertDialog
+import android.app.Dialog
 import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.view.Window
+import android.widget.Button
+import android.widget.TextView
 import androidx.fragment.app.Fragment
+import com.evdesporapp.R
 import com.evdesporapp.databinding.FragmentHomeBinding
 
 
@@ -18,6 +24,25 @@ class HomeFragment : Fragment() {
         _binding = FragmentHomeBinding.inflate(inflater, container, false)
         return binding.root
     }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+
+        val builder = AlertDialog.Builder(requireContext(),R.style.CustomAlertDialog)
+            .create()
+
+        val view = layoutInflater.inflate(R.layout.dialog_message_layout,null)
+        builder.setView(view)
+
+        binding.addWaterButton.setOnClickListener {
+            builder.dismiss()
+        }
+        builder.setCanceledOnTouchOutside(false) //dış ekrana dokunarak iptal etmeyi kapat.
+        builder.show()
+
+
+    }
+
 
 
     override fun onDestroyView() {
