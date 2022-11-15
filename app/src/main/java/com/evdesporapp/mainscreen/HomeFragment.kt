ffbceaee37237522a1958a63e211237e3c0fee72
Hilal <hilaltokgoz98@gmail.com>
2022-11-15 13:17:09 +0300

    customDialog

--- app/src/main/java/com/evdesporapp/mainscreen/HomeFragment.kt
+++ app/src/main/java/com/evdesporapp/mainscreen/HomeFragment.kt
@@ -1,14 +1,10 @@
 package com.evdesporapp.mainscreen
 
 import android.app.AlertDialog
-import android.app.Dialog
 import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
-import android.view.Window
-import android.widget.Button
-import android.widget.TextView
 import androidx.fragment.app.Fragment
 import com.evdesporapp.R
 import com.evdesporapp.databinding.FragmentHomeBinding
@@ -33,19 +29,20 @@ class HomeFragment : Fragment() {
         }
     }
 
-    fun showCustomDialog(){
-        val builder = AlertDialog.Builder(requireContext(),R.style.CustomAlertDialog)
+    private fun showCustomDialog() {
+        val builder = AlertDialog.Builder(requireContext(), R.style.CustomAlertDialog)
             .create()
-
-        val view = layoutInflater.inflate(R.layout.dialog_message_layout,null)
+        val view = layoutInflater.inflate(R.layout.dialog_message_layout, null)
         builder.setView(view)
-
         binding.addWaterButton.setOnClickListener {
             builder.dismiss()
         }
         builder.setCanceledOnTouchOutside(false) //dış ekrana dokunarak iptal etmeyi kapat.
         builder.show()
+    }
+    fun addWater(){
 
+        TODO("ekle butonuna basınca miktar kadar progress bar çubuğu artacak")
     }
 
 
