1198762824fc92ea6b9d39d823de61ccbe86ed79
Hilal <hilaltokgoz98@gmail.com>
2023-02-01 16:57:46 +0300

    notificationFragment

---
+++ app/src/main/java/com/evdesporapp/model/NotificationVM.kt
@@ -0,0 +1,14 @@
+package com.evdesporapp.model
+
+
+// Code with ❤️
+//┌──────────────────────────┐
+//│ Created by Hilal TOKGOZ       │
+//│ ──────────────────────── │
+//│ hilaltokgoz98@gmail.com                 │            
+//│ ──────────────────────── │
+//│ 1.02.2023                  │
+//└──────────────────────────┘
+
+data  class NotificationVM(val notificationImage:Int, val notificationText:String, val notificationClock:String) {
+}
--- app/src/main/java/com/evdesporapp/navigationdrawer/NotificationFragment.kt
+++ app/src/main/java/com/evdesporapp/navigationdrawer/NotificationFragment.kt
@@ -5,7 +5,11 @@ import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import androidx.fragment.app.Fragment
+import androidx.recyclerview.widget.GridLayoutManager
 import com.evdesporapp.databinding.FragmentNotificationBinding
+import com.evdesporapp.model.FitHomeAdapter
+import com.evdesporapp.model.FitHomeVM
+import com.evdesporapp.model.FitRecipeHomeList
 
 class NotificationFragment : Fragment() {
     private var _binding: FragmentNotificationBinding? = null
@@ -21,8 +25,12 @@ class NotificationFragment : Fragment() {
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
+        val recyclerView= binding.rvNotification
+        recyclerView.layoutManager= GridLayoutManager(requireContext(),2)
+        val adapter = FitHomeAdapter(FitRecipeHomeList.getFitRecipe() as ArrayList<FitHomeVM>)
+        recyclerView.adapter=adapter
 
-        val notification= binding.notificationListItemText
+
 
         TODO()
         //listedeki elemanları random ver, eğer exercise ise exercise iconunu, su ise water iconunu imageView e ata.
