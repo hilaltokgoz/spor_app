08916c2551c515ced114487f64fea06c117f840a
Hilal <hilaltokgoz98@gmail.com>
2023-02-02 18:01:33 +0300

    calculateWater

--- app/src/main/java/com/evdesporapp/navigationdrawer/CalculateWaterFragment.kt
+++ app/src/main/java/com/evdesporapp/navigationdrawer/CalculateWaterFragment.kt
@@ -1,10 +1,10 @@
 package com.evdesporapp.navigationdrawer
 
 import android.os.Bundle
-import android.view.LayoutInflater
-import android.view.View
-import android.view.ViewGroup
+import android.view.*
+import android.widget.ArrayAdapter
 import androidx.fragment.app.Fragment
+import com.evdesporapp.R
 import com.evdesporapp.databinding.FragmentCalculateWaterBinding
 
 class CalculateWaterFragment : Fragment() {
@@ -15,8 +15,14 @@ class CalculateWaterFragment : Fragment() {
         savedInstanceState: Bundle?
     ): View? {
         _binding = FragmentCalculateWaterBinding.inflate(inflater, container, false)
+
+        val activityTypes=resources.getStringArray(R.array.activities_types)
+        val arrayAdapter= ArrayAdapter(requireContext(),R.layout.dropdown_item,activityTypes)
+        binding.autoCompleteTextView.setAdapter(arrayAdapter)
+
         return binding.root
     }
+
 
 
     override fun onDestroyView() {
