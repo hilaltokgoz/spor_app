6eb0e2423c564704ae4ad799478cbedc1892cd32
Hilal <hilaltokgoz98@gmail.com>
2023-02-04 13:35:54 +0300

    calculateWater finish

--- app/src/main/java/com/evdesporapp/navigationdrawer/CalculateWaterFragment.kt
+++ app/src/main/java/com/evdesporapp/navigationdrawer/CalculateWaterFragment.kt
@@ -1,11 +1,16 @@
 package com.evdesporapp.navigationdrawer
 
+import android.graphics.Color
 import android.os.Bundle
 import android.view.*
 import android.widget.ArrayAdapter
+import androidx.compose.material.MaterialTheme.colors
+import androidx.compose.material.TextFieldDefaults
+import androidx.compose.runtime.Composable
 import androidx.fragment.app.Fragment
 import com.evdesporapp.R
 import com.evdesporapp.databinding.FragmentCalculateWaterBinding
+import org.w3c.dom.Text
 
 class CalculateWaterFragment : Fragment() {
     private var _binding: FragmentCalculateWaterBinding? = null
