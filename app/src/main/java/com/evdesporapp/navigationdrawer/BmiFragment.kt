40cd47df8c34a62d4d9214d6e581ddc24250a8a4
Hilal <hilaltokgoz98@gmail.com>
2023-02-05 13:48:59 +0300

    calculateWater finish

--- app/src/main/java/com/evdesporapp/navigationdrawer/BmiFragment.kt
+++ app/src/main/java/com/evdesporapp/navigationdrawer/BmiFragment.kt
@@ -6,6 +6,8 @@ import android.view.View
 import android.view.ViewGroup
 import androidx.fragment.app.Fragment
 import com.evdesporapp.databinding.FragmentBmiBinding
+import java.lang.Math.pow
+import kotlin.math.pow
 
 class BmiFragment : Fragment() {
     private var _binding: FragmentBmiBinding? = null
@@ -20,7 +22,24 @@ class BmiFragment : Fragment() {
         return binding.root
 
     }
-
+//
+//    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+//        super.onViewCreated(view, savedInstanceState)
+//        val line= binding.imageViewLine
+//        val height=binding.editTextHeight.text
+//        val weight=binding.editTextWeight.text
+//        val result=height/(weight*weight)
+//        binding.buttonCalculateWater.setOnClickListener {
+//            when(it){
+//                result<=18.5 -> binding.textViewBmiBox1 //line burada   //zayıf
+//               18.5 <=result<=24.9 -> binding.textViewBmiBox2    //normal
+//               25.0 <=result<=29.9 -> binding.textViewBmiBox3    //kilolu
+//                30<=result<=34.9 -> binding.textViewBmiBox4     //obezite
+//                result>=35 -> binding.textViewBmiBox5         //aşırı obez
+//            }
+//
+//        }
+//    }
 
     override fun onDestroyView() {
         super.onDestroyView()
