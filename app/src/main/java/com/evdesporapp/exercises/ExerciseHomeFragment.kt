da01901580a8b6c91e58aceb3d0c5d69af5bfeb6
Hilal <hilaltokgoz98@gmail.com>
2023-02-06 22:42:01 +0300

    exerciseListFragment started

--- app/src/main/java/com/evdesporapp/exercises/ExerciseHomeFragment.kt
+++ app/src/main/java/com/evdesporapp/exercises/ExerciseHomeFragment.kt
@@ -6,8 +6,8 @@ import android.view.View
 import android.view.ViewGroup
 import android.widget.ArrayAdapter
 import androidx.fragment.app.Fragment
+import androidx.navigation.fragment.findNavController
 import androidx.recyclerview.widget.LinearLayoutManager
-import com.evdesporapp.R
 import com.evdesporapp.databinding.FragmentExerciseHomeBinding
 import com.evdesporapp.model.FavExerciseAdapter
 import com.evdesporapp.model.FavExerciseList
@@ -36,17 +36,24 @@ class ExerciseHomeFragment : Fragment() {
         recyclerView.adapter = adapter
         addListViewItem()
 
-     //   val position= binding.exercisePartLV[0]
-     //   position.setBackgroundColor(Color.parseColor("#bcfd42"))
+        //Sayfa Yönlendirmesi
+        binding.textViewTitle.setOnClickListener {
+            val action =
+                ExerciseHomeFragmentDirections.actionExerciseHomeFragment2ToExerciseListFragment2()
+            findNavController().navigate(action)
+        }
+
+        //   val position= binding.exercisePartLV[0]
+        //   position.setBackgroundColor(Color.parseColor("#bcfd42"))
     }
 
     private fun addListViewItem() {
         val parts = arrayOf(
-            "  Sırt ","  Kol", "  Alt Karın","  Üst Karın","  Orta Karın", "  Kalça",
-            "  Üst Bacak","  Alt Bacak", "  Kardiyo", "  Esneme"
+            "  Sırt ", "  Kol", "  Alt Karın", "  Üst Karın", "  Orta Karın", "  Kalça",
+            "  Üst Bacak", "  Alt Bacak", "  Kardiyo", "  Esneme"
         )
         val arrayAdapter =
-            ArrayAdapter(requireContext(), android.R.layout.simple_list_item_1,parts)
+            ArrayAdapter(requireContext(), android.R.layout.simple_list_item_1, parts)
         binding.exercisePartLV.adapter = arrayAdapter
     }
 
