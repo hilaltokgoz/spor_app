535b103e4bd0338ab1575d723e83fd0689139985
Hilal <hilaltokgoz98@gmail.com>
2023-02-23 20:33:31 +0300

    exerciseListFragment started

--- app/src/main/java/com/evdesporapp/exercises/ExerciseListDetailFragment.kt
+++ app/src/main/java/com/evdesporapp/exercises/ExerciseListDetailFragment.kt
@@ -6,7 +6,9 @@ import android.view.View
 import android.view.ViewGroup
 import android.widget.Toast
 import androidx.constraintlayout.widget.ConstraintLayout
+import androidx.fragment.app.DialogFragment.STYLE_NORMAL
 import androidx.fragment.app.Fragment
+import com.evdesporapp.R
 import com.evdesporapp.base.BaseHideFragment
 import com.evdesporapp.databinding.FragmentExerciseListDetailBinding
 import com.google.android.material.bottomsheet.BottomSheetBehavior
@@ -16,44 +18,35 @@ class ExerciseListDetailFragment : Fragment()  {
     private var _binding:FragmentExerciseListDetailBinding?=null
     private val binding get() = _binding!!
 
+    private val bottomSheetView by lazy { findViewById<ConstraintLayout>(R.id.bottomSheet) }
     private lateinit var bottomSheetBehavior: BottomSheetBehavior<ConstraintLayout>
+
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
         _binding= FragmentExerciseListDetailBinding.inflate(inflater,container,false)
         val view=binding.root
+
+        bottomSheetBehavior = BottomSheetBehavior.from(bottomSheetView)
+        setBottomSheetVisibility(false)
+        setStyle(STYLE_NORMAL, R.style.BottomSheetDialog)
         return view
     }
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
-
-        //Bottom Sheet
-       // bottomSheetBehavior = BottomSheetBehavior.from(bottomSheet)
-
-//        bottomSheetBehavior.addBottomSheetCallback(object :
-//            BottomSheetBehavior.BottomSheetCallback() {
-//
-//            override fun onSlide(bottomSheet: View, slideOffset: Float) {
-//                // handle onSlide
-//            }
-//
-//            override fun onStateChanged(bottomSheet: View, newState: Int) {
-//                when (newState) {
-//                    BottomSheetBehavior.STATE_COLLAPSED -> Toast.makeText(requireContext(), "STATE_COLLAPSED", Toast.LENGTH_SHORT).show()
-//                    BottomSheetBehavior.STATE_EXPANDED -> Toast.makeText(requireContext(), "STATE_EXPANDED", Toast.LENGTH_SHORT).show()
-//                    BottomSheetBehavior.STATE_DRAGGING -> Toast.makeText(requireContext(), "STATE_DRAGGING", Toast.LENGTH_SHORT).show()
-//                    BottomSheetBehavior.STATE_SETTLING -> Toast.makeText(requireContext(), "STATE_SETTLING", Toast.LENGTH_SHORT).show()
-//                    BottomSheetBehavior.STATE_HIDDEN -> Toast.makeText(requireContext(), "STATE_HIDDEN", Toast.LENGTH_SHORT).show()
-//                    else -> Toast.makeText(requireContext(), "OTHER_STATE", Toast.LENGTH_SHORT).show()
-//                }
-//            }
-//        })
-
-
+     //bottom sheet
+        binding.button.setOnClickListener {
+            setBottomSheetVisibility(true)
+        }
 
     }
+    private fun setBottomSheetVisibility(isVisible: Boolean) {
+        val updatedState = if (isVisible) BottomSheetBehavior.STATE_EXPANDED else BottomSheetBehavior.STATE_COLLAPSED
+        bottomSheetBehavior.state = updatedState
+    }
+
     override fun onDestroyView() {
         super.onDestroyView()
         _binding = null
